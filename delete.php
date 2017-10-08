ef838dfebbfdfcdf4ff1f3743f3a03224471c702
Aaron Derby <aaronederby@gmail.com>
2017-10-07 23:43:18 -0700

    added the delete accound stuff

---
+++ delete.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+if (!$_SESSION['login'])
+	die("You are not logged in");
+if ($_POST['login'] && $_POST['passwd'] && $_POST['passwd2'] && $_POST['submit'])
+{
+	if ($_POST['passwd'] != $_POST['passwd2'])
+		die("Your passwords did not match <html><body><a href='deleteacc.html'>Go back</a></body></html>");
+	$account = unserialize(file_get_contents('./private/passwd'));
+	if ($account)
+	{
+		foreach ($account as $key => $arg)
+		{
+			if ($arg['login'] === $_POST['login'] && $arg['passwd'] === hash('whirlpool', $_POST['passwd']))
+			{
+				unset($account[$key]);
+				file_put_contents('./private/passwd', serialize($account));
+				die("Your account has been deleted <html><body><a href='deleteacc.html'>Go back</a></body></html>");
+			}
+		}
+	}
+}
+?>
